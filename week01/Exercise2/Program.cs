37a9cd975504d5209f9e99b71025c2447af9d753
aracelikyne <aracelikyne@gmail.com>
2025-05-07 16:37:12 -0500

    Finished Exercise 1,2,3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade: ");
+        string input = Console.ReadLine();
+        int grade  = int.Parse(input);
+        if (grade >= 90) {
+            Console.WriteLine("You received an A.");
+        } else if (grade >= 80) {
+            Console.WriteLine("You received a B.");
+        } else if (grade >= 70) {
+            Console.WriteLine("You received a C.");
+        } else if (grade >= 60) {
+            Console.WriteLine("You received a D.");
+        } else {
+            Console.WriteLine("You received an F.");
+        }
+        
     }
 }
